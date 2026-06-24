14446895fbd338651c2c5016b0d7f5a7990c9bd6
patelboss <83952959+patelboss@users.noreply.github.com>
2026-06-24 23:49:14 +0530

    Add 'Devanagari-Regular' to preferred font names

--- utils/summary.py
+++ utils/summary.py
@@ -126,6 +126,7 @@ def _iter_font_files(base: Path) -> list[Path]:
 
 def _find_devanagari_font() -> Path | None:
     preferred_names = (
+        "Devanagari-Regular",
         "devanagari-black",
         "nirmala",
         "mangal",
